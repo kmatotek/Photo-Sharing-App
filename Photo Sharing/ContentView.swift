f18f35514525fe3c9635d781dc18cfd9e9e1bc67
kmatotek <39473540+kmatotek@users.noreply.github.com>
2024-05-27 20:06:14 -0400

    Initial Commit

---
+++ Photo Sharing/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Photo Sharing
+//
+//  Created by Kadin Matotek on 5/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Photo Sharing/Photo_SharingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Photo_SharingApp.swift
+//  Photo Sharing
+//
+//  Created by Kadin Matotek on 5/27/24.
+//
+
+import SwiftUI
+
+@main
+struct Photo_SharingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
