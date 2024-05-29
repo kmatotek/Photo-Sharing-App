d7cf081174b00db14a12b8592dd7614009ce6a7b
kmatotek <kadinstitch@gmail.com>
2024-05-28 22:17:25 -0400

    Created login screen

---
+++ Photo Sharing/Login.swift
@@ -0,0 +1,18 @@
+//
+//  Login.swift
+//  Photo Sharing
+//
+//  Created by Kadin Matotek on 5/28/24.
+//
+
+import SwiftUI
+
+struct Login: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Login()
+}
