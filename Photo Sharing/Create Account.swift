023960e51fc293b6b3ed6f35b8de48d86fa65641
kmatotek <kadinstitch@gmail.com>
2024-05-30 12:33:05 -0400

    Created a Create Account Page and Page for first time users to create account or login

---
+++ Photo Sharing/Create Account.swift
@@ -0,0 +1,18 @@
+//
+//  Create Account.swift
+//  Photo Sharing
+//
+//  Created by Kadin Matotek on 5/30/24.
+//
+
+import SwiftUI
+
+struct Create_Account: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Create_Account()
+}
---
+++ Photo Sharing/FirstPageEver.swift
@@ -0,0 +1,18 @@
+//
+//  FirstPageEver.swift
+//  Photo Sharing
+//
+//  Created by Kadin Matotek on 5/30/24.
+//
+
+import SwiftUI
+
+struct FirstPageEver: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FirstPageEver()
+}
